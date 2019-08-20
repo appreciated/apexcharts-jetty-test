330f282a8697dc785151ea217221655fad2393af
Johannes Goebel <GoebelJohannes@gmx.net>
2019-08-20 13:05:41 +0200

    Initial commit

---
+++ src/main/java/com/github/test/MainView.java
@@ -0,0 +1,19 @@
+package com.github.test;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.router.Route;
+
+/**
+ * The main view contains a button and a click listener.
+ */
+@Route
+@PWA(name = "My Application", shortName = "My Application")
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+        add(new AreaChartExample());
+    }
+}
